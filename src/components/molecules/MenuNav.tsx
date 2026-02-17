1452afcabd75660fa479f408a3579959abf637b2
afrianska <5994999+afrianska@users.noreply.github.com>
2026-02-17 11:42:11 +0700

    updates: some adjustments

--- src/components/molecules/MenuNav.tsx
+++ src/components/molecules/MenuNav.tsx
@@ -1,7 +1,4 @@
 
-import { Iconmu } from '@iconmu/react'
-import { ArrowUpRightIcon } from '@iconmu/base-line'
-
 export default function Nav(defaultSelected: any) {
   let selected = defaultSelected['defaultSelected'];
 
@@ -9,9 +6,9 @@ export default function Nav(defaultSelected: any) {
     <ul>
       <li><a className={selected === 'start' ? 'selected' : 'unselected'} href='/'>Start</a></li>
       <li><a className={selected === 'study-cases' ? 'selected' : 'unselected'} href='/study-cases'>Study Cases</a></li>
-      <li><a className={`flex flex-row gap-1 unselected`} href='/links' target='_blank'>Links <span className='opacity-50'><Iconmu icon={ArrowUpRightIcon} /></span></a></li>
+      <li><a className={`flex flex-row gap-1 unselected`} href='/links' target='_blank'>Links <span className='opacity-50'><svg width="16" height="16" viewBox="0 0 40 40" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><g id="_26" data-name="26"><path d="M30,7H15a1,1,0,0,0,0,2H29.59L8.29,30.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L31,10.41V25a1,1,0,0,0,2,0V10A3,3,0,0,0,30,7Z" /></g></svg></span></a></li>
       <li><a className={`flex flex-row gap-1 unselected`} href='https://afrian.substack.com' target="_blank">
-        Writing <span className='opacity-50'><Iconmu icon={ArrowUpRightIcon} /></span>
+        Writing <span className='opacity-50'><svg width="16" height="16" viewBox="0 0 40 40" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><g id="_26" data-name="26"><path d="M30,7H15a1,1,0,0,0,0,2H29.59L8.29,30.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L31,10.41V25a1,1,0,0,0,2,0V10A3,3,0,0,0,30,7Z" /></g></svg></span>
       </a></li>
     </ul>
   );
